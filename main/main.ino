fe84e819562d0a930c91f1522e433360cb4fffbd
lloyd <lloydstorm1@gmail.com>
2023-11-24 12:24:49 +0000

    changes

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,10 @@
 // Author: Ben Taylor
 // University of Sheffield
 // Date: September 2021
+#include <NewPing.h>
+#define TRIGGER_PIN 22
+#define ECHO_PIN 23
+NewPing sonar(TRIGGER_PIN, ECHO_PIN);
 
 //class for storing all globals cause its good practise
 //ONLY USE BYTE FOR PINS AND NOTHING ELSE
@@ -20,6 +24,8 @@ class Globals{
       const byte truePin = 10;       // pin for true value in DC motor control (standby)
       const float corrFactor = 1.07;
 
+
+
       
 };
 Globals globals; //call the class 
@@ -35,6 +41,7 @@ void setup()
   pinMode(globals.leftDrivePin, OUTPUT);
   pinMode(globals.rightDrivePin, OUTPUT);
 
+
   // Initialize the serial port
   Serial.begin(9600);
 
@@ -42,16 +49,48 @@ void setup()
   digitalWrite(globals.truePin, HIGH);
 }
 
-void loop()
-{
 
-  rotate(100,1000,'L');
-  delay(1000);
-  setMotor(0,true,true);
-  setMotor(0,false,false);
-  //Serial.println("Compiled bitch");
-  // Wait 300ms
+/*
+  delay(50); // Wait 50ms between pings (this is the minimum recommended by NewPing library)
+  
+  unsigned int distance = sonar.ping_median(); // Get the distance in millimeters
+
+  if (distance == NO_ECHO) {
+    Serial.println("Error: No echo");
+  } else {
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" mm");
+  }
+*/
+void loop() {
+  rotate(150, 1550, 'R'); // Turn right 
+  Serial.println("rotate");
+
+  setMotor(150, true, true); // Set both motors forward 
+  setMotor(150, false, true); // Set both motors forward 
+  bool side = false;
+
+  while (!side) {
+    // While loop to stop the robot if the distance is below 866
+    unsigned int distance = sonar.ping_median();
+    Serial.println(distance);
+
+    // If the distance is less than or equal to 866, stop the motors and exit the loop
+    if (distance <= 866) {
+      setMotor(0, true, true); // Stop the motor in one direction
+      setMotor(0, false, true); // Stop the motor in the opposite direction
+      side = true;
+      break;
+    }
+  }
+  delay(5000);
+
+  // Exit the loop() function
+  return;
 }
+
+
 
 void setMotor(int pwmVal, bool leftMotor, bool forward)
 {
@@ -100,6 +139,23 @@ void rotate(int speed, long time, char direction) {
   // turn
   setMotor(speed, left, true);
   setMotor(speed, !left, false);
+  delay(time);
+  setMotor(0, true, true);
+  setMotor(0, false, false);
+}
+
+void straight(int speed, long time, char direction) {
+  //direction needs to be either 'F' or 'B' char input. 
+  bool left;
+  if (direction == 'F') {
+    left = true;
+  } else if (direction == 'B') {
+    left = false;
+  }
+
+  // turn
+  setMotor(speed, true, left);
+  setMotor(speed, false, true);
   delay(time);
   setMotor(0, true, true);
   setMotor(0, false, false);
