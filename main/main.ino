0d5e0865cf2ece065c329b0169a764047ce30e3e
lloyd <lloydstorm1@gmail.com>
2023-12-04 17:56:34 +0000

    include drawing

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,4 @@
-// TB6612FNG Driver Board Sample board
-//
-// Author: Ben Taylor
-// University of Sheffield
-// Date: September 2021
+#include <Servo.h>
 #include <NewPing.h>
 //class for storing all globals cause its good practise
 //ONLY USE BYTE FOR PINS AND NOTHING ELSE
@@ -23,20 +19,28 @@ class Globals{
       const byte leftDrivePin = 3;  // left drive PIN PWMB
       const byte rightDrivePin = 2; // right drive PIN PWMA
       const byte truePin = 51;       // pin for true value in DC motor control (standby)
-      const long rotate180 = 2000; 
-      const long rotate90 = rotate180/2 -30;
+      const byte bottomServoPin = 10; 
+      const byte middleServoPin = 7; 
+      const byte topServoPin = 8;
+      const byte flagServoPin = 9; 
+      const long rotate180 = 2520; 
+      const long rotate90 = 1260;
       const long widthTime = 3230; //distance from x to line 
-      const long echoOnLine = 1250;
-      const long echoToDrawingBoard = 300;
-      const float corrFactor = 1.06;
-
-
-
-
-      
+      const long echoOnLine = 1150;
+      const long echoToDrawingBoard = 1150;
+      const float corrFactor = 1.04; //increase for faster right motor       
 };
 Globals globals; //call the class 
 NewPing sonar(globals.frontTriggerPin, globals.frontEchoPin);
+
+Servo BottomServo;  // create servo object to control a servo
+Servo MiddleServo;
+Servo TopServo;
+Servo FlagsServo;
+int bottomPos = 0;    // variable to store the servo position
+int middlePos = 0;
+int topPos = 0;
+int flagsPog = 0;
 
 //NewPing sonar(globals.frontTriggerPin, globals.frontEchoPin); //doesn't work inside globals. 
 void setup()
@@ -55,6 +59,15 @@ void setup()
   pinMode(globals.lineFollowCenterPin, INPUT);
   pinMode(globals.lineFollowRightPin, INPUT); 
 
+
+  //servo stuff
+  BottomServo.attach(globals.bottomServoPin);  // attaches the servo on pin 9 to the servo object
+  MiddleServo.attach(globals.middleServoPin);
+  TopServo.attach(globals.topServoPin);
+  FlagsServo.attach(globals.flagServoPin);
+  //below cmds initially fold servo
+  armFold(); 
+  FlagsServo.write(0);
   // Initialize the serial port
   Serial.begin(9600);
 
@@ -95,6 +108,8 @@ void setup()
   }
 */
 void loop() {
+  delay(5000);
+  FlagsServo.write(0);
   //TURN1: initial 180
   rotate(100,globals.rotate180,'R'); //init rotate //2210 not good on full charge. 
   
@@ -109,10 +124,15 @@ void loop() {
   //straight(150,4000,'F'); //unsure if this time value is correct.
   stopIfEcho(globals.echoToDrawingBoard);
   //DELAY1: wait for drawing to finish. 
+
+  armExtend(); 
+  armSpin();
+  armFold(); 
+
   delay(2000); 
 
   //REVERSE1: move away from drawing board. 
-  straight(150,1000,'B'); // reverse 
+  //straight(150,1000,'B'); // reverse 
   
   //TURN3: turn to face red line.
   rotate(100,globals.rotate90,'L'); 
@@ -131,6 +151,9 @@ void loop() {
 
   //TURN5: face away and return to starting position.
   rotate(100,globals.rotate90,'R'); //turn to face bridge 
+
+  //RAISE FLAGS:
+  FlagsServo.write(180);
 
   delay(5000);
 }
@@ -220,4 +243,39 @@ void stopIfEcho(long time) {
       break;
     }
   }
+}
+
+
+void armFold(){
+    
+    middlePos = 180;
+    MiddleServo.write(middlePos);
+    bottomPos = 0;
+    BottomServo.write(bottomPos);
+    delay(3000);
+}
+
+void armExtend(){
+    
+    middlePos = 90;
+    MiddleServo.write(middlePos);
+    bottomPos = 70;
+    BottomServo.write(bottomPos);
+
+    delay(3000);
+}
+
+void armSpin(){
+    topPos = 0;
+    TopServo.write(topPos);
+    delay(2000);
+    for (int i = 0; i <= 180; i = i + 10){
+        topPos = i;
+        TopServo.write(topPos);
+        delay(50);
+    }
+    topPos = 50;
+    TopServo.write(topPos);
+    delay(2000);
+    
 }
